2d5af0752df05399fabe49a819b6b4bfb43187a3
lystosas <lystosas@gmail.com>
2023-10-25 19:58:51 -0500

    programa funcionando

--- generador.py
+++ generador.py
@@ -1,27 +1,89 @@
 import random
 
-print('*******GENERADOR DE CONTRASEÑAS***********')
+print("*******GENERADOR DE CONTRASEÑAS***********")
 
-minimo = int(input('Indique número mínimo de minusculas: '))
-# maximo = int(input('Indique número mínimo de mayusculas: '))
-# numerico = int(input('Indique número mínimo de caracteres númericos: '))
-# longitud = int(input('Indique longitud de la contraseña: '))
+minuscula = int(input("Indique número mínimo de minusculas: "))
+mayuscula = int(input("Indique número mínimo de mayusculas: "))
+numerico = int(input("Indique número mínimo de caracteres númericos: "))
+longitud = int(input("Indique longitud de la contraseña: "))
 
-abecedario = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+abecedario = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
 
-contrasena = []
+contraseña = []
+sumatoria = 0
+
 
 def numAleatorio():
-    return random.randint(0, len(abecedario) - 1)    
+    return random.randint(0, len(abecedario) - 1)
+
 
 def letra():
     return abecedario[numAleatorio()]
 
 
+while sumatoria <= longitud:
+    while minuscula > 0:
+        if sumatoria == longitud:
+            break
 
+        contraseña.append(letra())
+        minuscula -= 1
+        sumatoria += 1
 
-    letras = letra()
-    contrasena.append(letras)
+    while mayuscula > 0:
+        if sumatoria == longitud:
+            break
 
+        contraseña.append(letra().upper())
+        mayuscula -= 1
+        sumatoria += 1
 
-print(contrasena)
+    while numerico > 0:
+        if sumatoria == longitud:
+            break
+
+        contraseña.append(str(random.randint(1, 9)))
+        numerico -= 1
+        sumatoria += 1
+
+    if sumatoria != longitud:
+        minuscula, mayuscula, numerico = 1, 1, 1
+
+    if sumatoria == longitud:
+        break
+
+# Con random.shuffle desordeno el array
+random.shuffle(contraseña)
+
+# Con "".join(contraseña) extraigo los elemntos del array y creo una cadena
+clave = "".join(contraseña)
+
+print(clave)
+print(f"SU CONTRASEÑA ES: {contraseña}")
